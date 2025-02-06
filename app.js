633e4390b0ca45b8e036732cfd9ebdc0af27bf4a
Vib-UX <vibchess123@gmail.com>
2025-02-06 22:18:46 +0530

    x-auth passport init

---
+++ app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const TwitterStrategy = require("passport-twitter").Strategy;
+
+const app = express();
+
+// Session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport Twitter Strategy configuration
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      callbackURL: "http://localhost:3000/auth/twitter/callback",
+    },
+    function (token, tokenSecret, profile, done) {
+      // Here you can save the user profile to your database
+      return done(null, profile);
+    }
+  )
+);
+
+// Serialize user
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+// Deserialize user
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/twitter">Login with Twitter</a>');
+});
+
+app.get("/auth/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", { failureRedirect: "/" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
